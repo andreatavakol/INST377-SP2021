bd0a7d37adfd99b30d9325eff0094dc0b7e92342
Andrea Tavakol <andreatavakol@gmail.com>
2021-02-20 23:56:49 -0500

    EDIT: More Flappy Bird progress

--- public/flappy/script.js
+++ public/flappy/script.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded' , () => {
     let birdBottom = 100
     let gravity = 2
     let isGameOver = false
+    let gap = 400
 
     function startGame() {
         birdBottom -= gravity
@@ -35,10 +36,21 @@ document.addEventListener('DOMContentLoaded' , () => {
         let obstacleLeft = 500
         let obstacleBottom = randomHeight
         const obstacle = document.createElement('div')
+        const topObstacle = document.createElement('div')
+
+        if (!isGameOver) {
+            obstacle.classList.add('obstacle')
+            obstacle.classList.add('topObstacle')
+        }
+    
         obstacle.classList.add('obstacle')
         gameDisplay.appendChild(obstacle)
+
         obstacle.style.left = obstacleLeft + 'px'
+        topObstacle.style.left = obstacleLeft + 'px'
         obstacle.style.bottom = obstacleBottom + 'px'
+        topObstacle.style.bottom = obstacleBottom + gap + 'px'
+        
 
         function moveObstacle() {
             obstacleLeft -= 2
@@ -49,8 +61,10 @@ document.addEventListener('DOMContentLoaded' , () => {
                 gameDisplay.removeChild(obstacle)
             }
             if (obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 ||
+                birdBottom < obstacleBottom + 153 ||
                 birdBottom === 0) {
                 gameOver()
+                clearInterval(timerID)
             }
         }
         let timerID = setInterval(moveObstacle, 20)

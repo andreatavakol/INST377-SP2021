d1c46ef1aea8a66a077c7b806cb94d1d3394ce0e
Andrea Tavakol <andreatavakol@gmail.com>
2021-03-13 00:09:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,6 @@ app.route('/api')
     console.log('Form data in res.body', req.body);
     console.log('Now send something back to your client');
     // res.json({data: dataToSendToFrontEnd});
-    res.json("Hello World");
   });
 
 app.listen(port, async () => {
